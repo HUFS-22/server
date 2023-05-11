ac304a6b8c036475bd72169980fa7f409411d281
DAHLIACHOI <cyj000424@naver.com>
2023-05-12 02:48:37 +0900

    initial setting

---
+++ src/main/java/com/example/vwx/VwxApplication.java
@@ -0,0 +1,13 @@
+package com.example.vwx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VwxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VwxApplication.class, args);
+	}
+
+}
